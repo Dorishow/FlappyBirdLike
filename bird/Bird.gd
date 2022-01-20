fd2e24019ac9ac5c62ab40218e3d3c6cc14c4c53
João Almeida <69915069+Dorishow@users.noreply.github.com>
2022-01-20 04:17:48 -0300

    Refactoring bird class

--- bird/Bird.gd
+++ bird/Bird.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-const gravity = 800
-const jumpSpeed = 50
+const GRAVITY = 900
+const JUMPSPEED = 50
 var velocity = Vector2()
 var pressedJump
 var screensize = Vector2()
@@ -16,13 +16,8 @@ func _physics_process(delta):
 	clamp_position()
 
 func listen_input(): if(pressedJump): jump()
-
-func jump(): velocity.y = -jumpSpeed * (gravity / 2)
-
-func apply_gravity(): if !is_on_floor(): velocity.y += gravity
-
+func jump(): velocity.y = -JUMPSPEED * (GRAVITY / 2)
+func apply_gravity(): if !is_on_floor(): velocity.y += GRAVITY
 func apply_motion(delta): move_and_slide(velocity * delta, Vector2.UP)
-
 func updatePressedJump(): pressedJump = Input.is_action_just_pressed("ui_up")
-
 func clamp_position(): position.y = clamp(position.y, 0, screensize.y)
